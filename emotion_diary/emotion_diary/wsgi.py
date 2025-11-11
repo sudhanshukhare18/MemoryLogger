8a656ff15fc03a4fb4ee70aa26ce268615de7d3c
Sudhanshu Khare <sudhanshukhare021@gmail.com>
2025-11-11 08:08:16 +0000

    s

--- emotion_diary/emotion_diary/wsgi.py
+++ emotion_diary/emotion_diary/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emotion_diary.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotion_diary.emotion_diary.settings')
 
 application = get_wsgi_application()
--- emotion_diary/manage.py
+++ emotion_diary/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emotion_diary.settings")
+   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotion_diary.emotion_diary.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
