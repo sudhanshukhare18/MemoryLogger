77d2705609bb2f06fc71690832ced14b04c3ec83
Sudhanshu Khare <sudhanshukhare021@gmail.com>
2025-11-11 06:25:20 +0000

    success

--- emotion_diary/build.sh
+++ emotion_diary/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install --upgrade pip
 
+pip install torch==2.9.0 torchvision==0.19.0 torchaudio==2.9.0 --index-url https://download.pytorch.org/whl/cpu
 
 pip install -r requirements.txt
 
