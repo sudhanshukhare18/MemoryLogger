8500cb1f05147ec4e36561103aa8c106a8ac247c
Sudhanshu Khare <sudhanshukhare021@gmail.com>
2025-11-11 05:30:44 +0000

    s

--- emotion_diary/build.sh
+++ emotion_diary/build.sh
@@ -3,6 +3,8 @@
 set -o errexit  
 
 pip install --upgrade pip
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
